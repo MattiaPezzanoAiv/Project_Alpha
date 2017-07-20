e2df44fa45eef968d420fd6453c1dbf8d17083c4
MattiaPezzanoAiv <mattiapezzano7@gmail.com>
2017-07-20 20:00:32 +0200

    imported text mesh pro, preparing to create points text

---
+++ Assets/Scripts/PointsManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointsManager : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject player;
+    [SerializeField]
+    private Text textFieldOnCanvas;
+    [SerializeField]
+    private int pointsPerKill;
+    [SerializeField]
+    private int pointsPerExplosion;
+
+
+    private int points;
+    public int Points
+    {
+        get
+        {
+            return points;
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+		if(player == null)
+            player = GameObject.FindObjectOfType<PlayerShooter>().gameObject;
+        if (player == null)
+            throw new UnityException("PLAYER IN POINTS MANAGER MISSING REFERENCE");
+    }
+	
+	private void UpdatePoints()
+    {
+        textFieldOnCanvas.text = string.Format("Points: {0}", Points);
+    }
+    public void AddKillPoints()
+    {
+        points += pointsPerKill;
+    }
+    public void AddExplosionPoints()
+    {
+        points += pointsPerExplosion;
+    }
+}
