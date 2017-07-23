e9fef2bfcd7a151b6944c204e55ce9a76812ca0b
MattiaPezzanoAiv <mattiapezzano7@gmail.com>
2017-07-23 20:53:11 +0200

    created score scenes, and controller script

---
+++ Assets/Scripts/BestScoresController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BestScoresController : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject otherScoresParent;
+    private List<Text> otherScores;
+
+    void Awake()
+    {
+        otherScores = new List<Text>();
+        for (int i = 0; i < otherScoresParent.transform.childCount; i++)
+        {
+            Text scoreText = otherScoresParent.transform.GetChild(i).GetComponent<Text>();
+            otherScores.Add(scoreText);
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
